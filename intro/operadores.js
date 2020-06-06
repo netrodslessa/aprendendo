490d0f1d7f69599628cec40bdcc1bd81598311f1
Rodrigo Lessa <netrods@gmail.com>
2020-06-06 11:30:08 -0300

    resumo e exemplo sobre objetos

---
+++ intro/operadores.js
@@ -0,0 +1,19 @@
+// objeto é uma coleção de propriedades que recebe valores ou funcionalidades 
+// de alguma coisa 
+
+const aluno01 = {
+    nome: 'Rodrigo',
+    nota: 9.8,
+}
+
+const aluno2 = {
+    nome: 'João',
+    nota: 10
+}
+
+const aluno3 = {
+    nome: 'Márcia',
+    nota: 2
+}
+
+console.log((aluno01.nota + aluno2.nota + aluno3.nota) / 3 )
