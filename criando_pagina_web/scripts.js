6d99b2fd62dbc33af6a90edbc7884481fa1831d8
netrodslessa <netrods@gmail.com>
2020-06-22 08:50:21 -0300

    realizado a finalização da faze 2, iniciando a faze 3 e mudando o banch

--- criando_pagina_web/scripts.js
+++ criando_pagina_web/scripts.js
@@ -3,10 +3,14 @@ const cards = document.querySelectorAll('.card')
 
 for(let card of cards){
     card.addEventListener("click", ()=>{
+        const videoId = card.getAttribute("id")
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src=`https://youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector('.close-modal').addEventListener("click", ()=>{
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src=``
 })
+
